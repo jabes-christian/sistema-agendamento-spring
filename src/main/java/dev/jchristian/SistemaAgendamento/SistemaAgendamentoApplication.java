8044a584ee2a4cad49bfa259df7b37d0316d474d
Jabes Christian <jabescristian@gmail.com>
2026-02-05 22:06:07 -0300

    primeiro commit

---
+++ src/main/java/dev/jchristian/SistemaAgendamento/SistemaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package dev.jchristian.SistemaAgendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendamentoApplication.class, args);
+	}
+
+}
